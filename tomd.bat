52197287aeb8d921eb0f5d155c9838ae2a746fef
Pedro Jiménez <pedro.jimenez.2810@gmail.com>
2026-06-24 16:48:29 -0400

    Update tomd.bat

--- tomd.bat
+++ tomd.bat
@@ -91,7 +91,7 @@ Write-Host "Analizando $($allFiles.Count) archivos totales... ($totalFiles pasar
 $indexFile = Join-Path $destDir "_00_arbol_indice.md"
 $treeOutput = & tree.com $normalizedPath /F /A
 $treeContent = "# ÍNDICE DEL DIRECTORIO (Contexto Estructural)`r`n`r`n"
-$treeContent += "**Proyecto:** `$folderName`  `r`n"
+$treeContent += "**Proyecto:** $folderName  `r`n"
 $treeContent += "**Nota:** Este es el árbol original del proyecto. Más abajo se detallan los archivos excluidos.`r`n`r`n"
 $treeContent += "```text`r`n"
 $treeContent += ($treeOutput -join "`r`n")
